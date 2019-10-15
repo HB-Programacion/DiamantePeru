bb338748ede0d69b99710c872a176ef27d2996d6
yenifergalarza <yeniferalexandra2000@hotmail.com>
2019-10-15 11:34:01 -0500

    mas estilos

--- my-app/src/components/Convenio.js
+++ my-app/src/components/Convenio.js
@@ -4,19 +4,19 @@ const Convenio = () => {
     return (
         <div className="animated fadeIn">
             <div className="fndo-img1 mb-0">
-                <div className="w100 pad-2 blue-background">
-                    <div className="w45 w100-xs center color-white"> <label className="spacing4 ">CONVENIO</label>
+                <div className="w100 pad-5-xs pad-2 blue-background">
+                    <div className="w45 w100-xs center color-white text-centered"> <label className="spacing4 ">CONVENIO</label>
                     <p className="text-centered">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua </p>
-                    <a className="" >Leer mas</a></div>
+                    <a className="text-centered" >Leer mas</a></div>
                    
                 </div>
             </div>
             <div className="gray"> 
                 <div className="fndo-img2 rigth">
-                    <div className="w35 gray-xs w87-xs gray-text-centered pad-3">
-                        <label className="spacing4">DIAMANTE<br/> PERU</label>
-                        <p className="gray-text-centered pad-14">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
-                        <a className="" >Leer mas</a> 
+                    <div className="w35 gray-xs w87-xs gray-text-centered  pad-14">
+                        <label className="spacing4"> <span className="fnt-w-bold">DIAMANTE</span><br/> PERÚ</label>
+                        <p className="gray-text-centered ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </p>
+                        <a className="" >Leer mas <span className="arrow-see-more">></span></a> 
                     </div>
                 </div>
             </div>
