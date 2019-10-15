ceb7c0380f02f33fe3530ac605d3aff8d672ba90
NardaC <nardac6@gmail.com>
2019-10-15 17:13:34 -0500

    cambios no

--- my-app/src/components/Convenio.js
+++ my-app/src/components/Convenio.js
@@ -5,11 +5,7 @@ const Convenio = () => {
         <div className="animated fadeIn">
             <div className="fndo-img1 mb-0">
                 <div className="w100 pad-5-xs pad-2 blue-background">
-<<<<<<< HEAD
-                    <div className="w45 w100-xs center color-white text-centered"> <label className="spacing4 "><b className="text-size-em">CONVENIO</b></label>
-=======
                     <div className="w45 w100-xs center color-white text-centered"> <label className="spacing4 text-1-5 bolder-xs ">CONVENIO</label>
->>>>>>> 19da5c828a925eadc0c92e3134ac54301b02d59f
                     <p className="text-centered">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua </p>
                     <a className="text-centered" >Leer mas</a></div>
                    
