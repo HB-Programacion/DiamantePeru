99b1d0da7fe9678f642e9e787ba3efb086a7e941
yenifer alexandra Galarza pezo <yeniferalexandra2000@hotmail.com>
2019-10-21 08:51:22 -0500

    estructura base con bugs

--- my-app/src/components/Joyas.jsx
+++ my-app/src/components/Joyas.jsx
@@ -4,8 +4,36 @@ import React from "react";
 const Joyas = () =>{
     return (
 <>
-<div className="container">
+<div className="container w100">
+    <div className="row">
+        <div className="container-text-joyas  col align-self-center">
+            <div className="container ">
+                <div className="row">
+                    <div className="col align-self-center w50 ">
+                          <h1 class="center">JOYAS</h1>
+      <p class="center ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam mollitia sit excepturi quos, doloremque eum maiores vitae dicta nihil animi expedita officia sequi ducimus molestias neque necessitatibus amet. Harum, reiciendis.
+      </p>
+                    </div>
+                </div>
+                
+            </div>
     
+  </div>  
+    </div>
+
+
+
+   <div className="container-img-joyas row justify-content-md-center">
+   <div className="filterAndSearch col">
+    <label> Buscar por <input type="text" id="NameOfJewerly" name="Name" /></label>
+    </div>
+    <div className="col-9">
+
+    </div>
+  
+
+</div>
+
 </div>
 </>
     )};
