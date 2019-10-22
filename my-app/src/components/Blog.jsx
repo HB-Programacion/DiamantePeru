2939659d95b48ba43645e555a6256ccdfb58c291
NardaC <nardac6@gmail.com>
2019-10-22 15:36:02 -0500

    blog avances

--- my-app/src/components/Blog.jsx
+++ my-app/src/components/Blog.jsx
@@ -15,16 +15,48 @@ const Blog = () =>{
                         <a className="leer-mas-blog mt-4">Leer más ></a>
                     </div>
             {/* </div> */}
-            <div class="container">
-  <div class="row">
-    <div class="col-6">col</div>
-    <div class="col-6">col</div>
-    <div class="col-6">col</div>
-    <div class="col-6">col</div>
-  </div>
-  
-</div>
-           
+            <div class="container p-5">
+                <div class="row">
+                    <div class="col-sm-5 col-md-6 mb-5">
+                        <div class="card" style={{width: "18 rem"}}>
+                            <img src={foto1} class="w100" alt="..."/>
+                            <div class="card-body pb-3">
+                                <h5 class="card-title mt-4 pl-5 ">CLUB DIAMANTE</h5>
+                                <p class="card-text pl-5 pr-5">Agradecemos tu confianza en nosotros y decidimos premiar esa confianza con diversos beneficios para brindarte que sean perfectos para ti.</p>
+                                <hr class="my-3"/>
+                                <a className="pl-5">LEER MAS<i class="fas fa-chevron-right arrow"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-5 col-md-6 mb-5">
+                        <div class="card" style={{width: "18 rem"}}>
+                            <img src={foto1} class="w100" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-5 col-md-6 mb-5">
+                        <div class="card" style={{width: "18 rem"}}>
+                            <img src={foto1} class="w100" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-5 col-md-6 mb-5">
+                        <div class="card" style={{width: "18 rem"}}>
+                            <img src={foto1} class="w100" alt="..."/>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>   
        </div>
      </Route>
 
