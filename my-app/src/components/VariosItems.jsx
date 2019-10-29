70c9d2b72dff2b6b9bd979a20e981d6de47cc44d
yenifer alexandra Galarza pezo <yeniferalexandra2000@hotmail.com>
2019-10-29 17:37:25 -0500

    aceptando cambios

--- my-app/src/components/VariosItems.jsx
+++ my-app/src/components/VariosItems.jsx
@@ -3,25 +3,30 @@ import React ,{useState,useEffect} from "react";
 import Item from './Item'
 
 const Varios= ({arrofJoyas})=>{
-  const [longitud,setlongitud]= useState([]);
-setlongitud([...arrofJoyas]);
+
+  let contenido;
+  if (arrofJoyas.length !==  0 ) {
+    contenido =    <div className="row" >
+    {arrofJoyas.map(prod => (
+   <Item
+        // key={prod.id}
+        dsc={prod.DESCRIPCIÓN}
+        nro={prod.CÓDIGO}
+        tipo={prod.Categoría}
+        color={prod.Color}
+        img={prod.imagen}
+      />
+    ))}
+     
+  </div>;
+  } else {
+    contenido = <div className="row">
+      <h1>Para antender su pedido,sírvase a dejar su correo y nos pondremos en contacto a la brevedad</h1>
+    </div>;
+  }
     return (
       <div className="container">
-        <div className="row" >
-          {arrofJoyas.map(prod => (
-         <Item
-              // key={prod.id}
-              dsc={prod.DESCRIPCIÓN}
-              nro={prod.CÓDIGO}
-              tipo={prod.Categoría}
-              color={prod.Color}
-              img={prod.imagen}
-            />
-          ))}
-              {longitud.lenght === undefined  && (
-           <h1> vacio estoy</h1>
-                )}
-        </div>
+     {contenido}
       </div>
     )
 };
