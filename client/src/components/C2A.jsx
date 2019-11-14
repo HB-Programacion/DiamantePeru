dd1b058a883f0feed691929c0f77881b552729d8
yenifer alexandra Galarza pezo <yeniferalexandra2000@hotmail.com>
2019-11-14 10:52:06 -0500

    cambios

---
+++ client/src/components/C2A.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const C2A = ()=>{
+return( 
+<a href="https://api.whatsapp.com/send?phone=5198053627&text=Hola%21%20Quisiera%20m%C3%A1s%20informaci%C3%B3n%20sobre%20Joyas%20."
+ className="float" target="_blank">
+<i className="fab fa-whatsapp"></i>
+</a>);
+}
+
+export default C2A;
