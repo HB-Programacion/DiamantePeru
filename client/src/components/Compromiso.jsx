6ceac03e5d8104ec5f1d388add40512951a922ab
yenifer alexandra Galarza pezo <yeniferalexandra2000@hotmail.com>
2019-12-05 18:07:19 -0500

    cambios maste2

--- client/src/components/Compromiso.jsx
+++ client/src/components/Compromiso.jsx
@@ -8,7 +8,7 @@ import moreDiamante from '../img/diamantes.png'
 
 const Compromiso = () => {
     return (
-        <div className="w100 pt-5 pb-5 flex flexCenter flex-column animated fadeIn"> 
+        <div className="w100 pt-5 pb-5 flex flexCenter flex-column fondoCompromiso animated fadeIn"> 
         <Helmet>
             <title>Compromiso - Diamante Perú</title>
             <meta name="description" content="Diamantes certificados por el Laboratorio mas prestigioso y famoso del mundo, Gemological Institute of America (GIA)" />
@@ -16,11 +16,11 @@ const Compromiso = () => {
         </Helmet>   <div>    <h1 className="p-3 flex flexCenter text-size27 "><b>ANILLOS DE COMPROMISO</b></h1>
         <p className="center">Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
     </div>
-            <div className="flex-wrap-xs flex mt-5 mb-5rem flexCenter">
+            <div className="flex-wrap-xs flex mt-5 mb-5rem flexCenter ">
                     
                 <Link to="/compromiso/diamante" className="w-auto borderBold m-2 mt-3 mb-3 m4-xs"  >
                     <img src={oneDiamante} alt="un Diamanate"className="w100 h100 box"/>
-                    <button className="joya">DIAMANTE</button>
+                    <button className="joya">SOLITARIO</button>
                 </Link>
                 <Link to="/compromiso/diamantes" className="w-auto borderBold m-2 mt-3 mb-3 m4-xs" >
                     <img src={moreDiamante}  alt="muchos Diamantes" className="w100 h100 box"/>
