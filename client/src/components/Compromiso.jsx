553c225dd3cae3966053940fb21e91e29b2d2c59
NardaC <nardac6@gmail.com>
2019-12-05 10:04:40 -0500

    home slint

--- client/src/components/Compromiso.jsx
+++ client/src/components/Compromiso.jsx
@@ -7,26 +7,23 @@ import oneDiamante from '../img/diamante.png'
 import moreDiamante from '../img/diamantes.png'
 
 const Compromiso = () => {
-  
     return (
-       
-        <div className="w100 pt-5 pb-5 flex flexCenter animated fadeIn"> <Helmet>
-        <title>Compromiso - Diamante Perú</title>
-        <meta name="description" content="Diamantes certificados por el Laboratorio mas prestigioso y famoso del mundo, Gemological Institute of America (GIA)" />
-        <meta name="keywords" content="Diamante,diamante,joyas,diamante peru,diamante perú,Mantenimiento" />
-    </Helmet>
-     
-                <div className="flex-wrap-xs flex mt-5 mb-5rem flexCenter">
-                    <Link to="/compromiso/diamantes" className="w-auto borderBold m-2 mt-3 mb-3 m4-xs"  >
-                        <img src={oneDiamante} alt="un Diamanate"className="w100 h100 box"/>
-                        <button className="joya">DIAMANTE</button>
-                    </Link>
-                    <Link to="/compromiso/diamante" className="w-auto borderBold m-2 mt-3 mb-3 m4-xs" >
-                        <img src={moreDiamante}  alt="muchos Diamanates" className="w100 h100 box"/>
-                        <button className="joya" ><span>+</span> DIAMANTES</button>
-                    </Link>
-                </div>
-          
+        <div className="w100 pt-5 pb-5 flex flexCenter animated fadeIn"> 
+        <Helmet>
+            <title>Compromiso - Diamante Perú</title>
+            <meta name="description" content="Diamantes certificados por el Laboratorio mas prestigioso y famoso del mundo, Gemological Institute of America (GIA)" />
+            <meta name="keywords" content="Diamante,diamante,joyas,diamante peru,diamante perú,Mantenimiento" />
+        </Helmet>
+            <div className="flex-wrap-xs flex mt-5 mb-5rem flexCenter">
+                <Link to="/compromiso/diamante" className="w-auto borderBold m-2 mt-3 mb-3 m4-xs"  >
+                    <img src={oneDiamante} alt="un Diamanate"className="w100 h100 box"/>
+                    <button className="joya">DIAMANTE</button>
+                </Link>
+                <Link to="/compromiso/diamantes" className="w-auto borderBold m-2 mt-3 mb-3 m4-xs" >
+                    <img src={moreDiamante}  alt="muchos Diamantes" className="w100 h100 box"/>
+                    <button className="joya" ><span>+</span> DIAMANTES</button>
+                </Link>
+            </div>
         </div>
     )
 }
--- client/src/components/Compromiso1.jsx
+++ client/src/components/Compromiso1.jsx
@@ -6,15 +6,16 @@ import CarrouselOne from './CarrouselOne';
 const Compromiso1 =()=>{
     const CompromisoSolo = arrJoyas.filter(e => e.Categoría==="Solitario" && e.Sección === "Novios/Anillo de Compromiso");
 return (
-    <div className="w100 pt-5 pb-5 flex flexCenter animated fadeIn"> <Helmet>
-    <title>Compromiso - Diamante Perú</title>
-    <meta name="description" content="Diamantes certificados por el Laboratorio mas prestigioso y famoso del mundo, Gemological Institute of America (GIA)" />
-    <meta name="keywords" content="Diamante,diamante,joyas,diamante peru,diamante perú,Mantenimiento" />
-</Helmet>
-    <div className="">
-                
-                <CarrouselOne arrofJoyas={CompromisoSolo}  title={"DIAMANTE"}></CarrouselOne>
-                 </div></div>
-)
+    <div className="w100 pt-5 pb-5 flex flexCenter animated fadeIn"> 
+        <Helmet>
+            <title>Compromiso - Diamante Perú</title>
+            <meta name="description" content="Diamantes certificados por el Laboratorio mas prestigioso y famoso del mundo, Gemological Institute of America (GIA)" />
+            <meta name="keywords" content="Diamante,diamante,joyas,diamante peru,diamante perú,Mantenimiento" />
+        </Helmet>
+        <div className="">
+            <CarrouselOne arrofJoyas={CompromisoSolo}  title={"DIAMANTE"}></CarrouselOne>
+        </div>
+    </div>
+    )
 }
 export default Compromiso1;
