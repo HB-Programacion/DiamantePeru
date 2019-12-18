2f23077a8df3f08e17b1a43837172885c5826bfe
NardaC <nardac6@gmail.com>
2019-12-18 10:23:49 -0500

    cambio

--- client/src/components/Beneficios.jsx
+++ client/src/components/Beneficios.jsx
@@ -33,22 +33,20 @@ const Beneficios = () => {
                   <li data-target="#multi-item-example" data-slide-to="4"></li>
                 </ol>
                 <div className="carousel-inner carousel-inner1 " role="listbox">
-                                <div className="carousel-item active">
+                  <div className="carousel-item active">
                     <div className="row">
                       <div className="col-md-12  cont-ben">
                         <div className="mb-2  flex flexWrap">
                           <figure className="w50 allCenter align-items-baseline  convenioResImg">
                             <img className=" w70 rounded-circle  ben-round" src={imgconvenio5} alt="Beneficio 2"/>
                           </figure>
-                       <div className="card-body w50 p-3 convenioRes geosansLight">
+                       <div className="card-body w50 p-3  convenioRes geosansLight">
                             <h4 className="card-title">SOCIOS CLUB DIAMANTE </h4>
-
-                   
                             <p className="card-text font-size-1">Por tu compra en Diamante Perú obtienes:</p>
-                            <p className="interline1 text-size12 justify">
-                            Forma parte del Club Diamante y descubre todos
-los beneficios, sólo debes realizar una compra en Diamante Perú e inscribirte en nuestro
-formulario web con tu DNI en el siguiente link : <a className="text-center option-menu raleway-medium-italic nav-link " href=" http://bit.ly/2XxDMLA">CLick Aqui</a>
+                            <p className="interline1 font-size-1rem justify">
+                              Forma parte del Club Diamante y descubre todos
+                              los beneficios, sólo debes realizar una compra en Diamante Perú e inscribirte en nuestro
+                              formulario web con tu DNI en el siguiente link : <a className="text-center option-menu raleway-medium-italic nav-link " href=" http://bit.ly/2XxDMLA">CLick Aqui</a>
                             </p>
                             <hr className="my-4"/>
                             <p className="interline1 text-size07  raleway-medium justify">
@@ -57,9 +55,7 @@ formulario web con tu DNI en el siguiente link : <a className="text-center optio
                                 La promoción puede ser transferible a un familiar directo (hijo, hija, hermana, papá y mamá), solo presentando el voucher de compra en DIAMANTE PERÚ. Para acceder al beneficio, el cliente y/o beneficiario debe registrarse en el formulario antes para solicitar la cita dando en referencia su nombre para poder corroborar que son clientes de DIAMANTE PERÚ. Esta promoción es válida con pago en efectivo o depósito en cuenta.
                             
                             </p>
-                          </div>
-                      
-                    
+                          </div> 
                      </div>
                       </div>
                     </div>
@@ -71,7 +67,7 @@ formulario web con tu DNI en el siguiente link : <a className="text-center optio
                           <figure className="w50 allCenter align-items-baseline  convenioResImg">
                             <img className=" w70 rounded-circle  ben-round" src={imgconvenio1} alt="Beneficio 2"/>
                           </figure>
-                          <div className="card-body w50 p-3 convenioRes geosansLight">
+                          <div className="card-body w50 p-3  convenioRes geosansLight">
                             <h4 className="card-title">CARMEN SEMINARIO LA ATELIER</h4>
                             <p className="card-text font-size-1">Por tu compra en Diamante Perú obtienes:</p>
                             <ul className="interline1 ml-4 font-size-1rem">
@@ -105,7 +101,7 @@ formulario web con tu DNI en el siguiente link : <a className="text-center optio
                           <figure className="w50 allCenter align-items-baseline  convenioResImg">
                             <img className=" w70 rounded-circle  ben-round" src={imgconvenio3} alt="Beneficio 2"/>
                           </figure>
-                          <div className="card-body w50 p-3 convenioRes geosansLight">
+                          <div className="card-body w50 p-3  convenioRes geosansLight">
                           <h4 className="card-title"> 
                           EL TALLER BY CARMEN SEMINARIO <br/>
                           ALIANZA COMERCIAL - CARMEN SEMINARIO y CLUB DIAMANTE PERU</h4>
@@ -140,15 +136,6 @@ formulario web con tu DNI en el siguiente link : <a className="text-center optio
                               AV. JAVIER PRADO ESTE 2456 - 3ER. PISO - SAN BORJA</p>
                               
                           </div>
-
-
-
-                         
-
-
-
-
-                       
                          </div>
                       </div>
                     </div>
@@ -160,11 +147,11 @@ formulario web con tu DNI en el siguiente link : <a className="text-center optio
                           <figure className="w50 allCenter convenioResImg align-items-baseline ">
                             <img className=" w70 rounded-circle  ben-round" src={imgconvenio2} alt="Beneficio 2"/>
                           </figure>
-                          <div className="card-body w50 p-3 convenioRes geosansLight">
+                          <div className="card-body w50 p-3  convenioRes geosansLight">
                             <h4 className="card-title">AGENCIA DE VIAJES GLOBAL NET</h4>
-                            <p className="card-text font-size-1">Por tu compra en Diamante Perú obtienes:</p>
+                            <p className="card-text font-size-1rem">Por tu compra en Diamante Perú obtienes:</p>
                             <ul className="interline1 ml-4 font-size-1rem">
-  
+
                               <li>10% De Descuento En servicios de alojamiento y traslados a los Destinos del Caribe.</li>
                               <li>15% De Descuento En tarjetas de asistencia al viajero.</li>
                              
@@ -192,11 +179,6 @@ formulario web con tu DNI en el siguiente link : <a className="text-center optio
                       </div>
                     </div>
                   </div>
-
-
-                 
-
-
               <div className="carousel-item">
                     <div className="row">
                       <div className="col-md-12  cont-ben">
@@ -204,7 +186,7 @@ formulario web con tu DNI en el siguiente link : <a className="text-center optio
                           <figure className="w50 allCenter align-items-baseline  convenioResImg">
                             <img className=" w70 rounded-circle  ben-round" src={imgconvenio4} alt="Beneficio 2"/>
                           </figure>
-                          <div className="card-body w50 p-3 convenioRes geosansLight">
+                          <div className="card-body w50 p-3  convenioRes geosansLight">
                             <h4 className="card-title">PAGOS SIN INTERESES - BBVA</h4>
                             <p className="card-text font-size-1"> BBVA: Compra la joya perfecta hasta en 03 meses sin intereses.</p>
                             
