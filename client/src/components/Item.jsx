af5015e9860d5844be664372c08d411725c4b152
NardaC <nardac6@gmail.com>
2020-01-14 19:36:08 -0500

    toda oacsion

--- client/src/components/Item.jsx
+++ client/src/components/Item.jsx
@@ -6,14 +6,13 @@ const Item = ({nro,tipo,color,dsc,img}) =>{
   return (
       <>
       <Link 
-        className="item-container-total text-decoration-none text-reset col-6 col-sm-4  mb-2 option-menu nav-link " 
+        className="item-container-total text-decoration-none text-reset col-12 col-sm-4  mb-2 option-menu nav-link " 
         to={ `/producto/${tipo}/${nro}`}
       >
         <div className="card background-carrousel-ring border-carrousel" key={nro}>
           <img src={img} className="card-img-top cardImgItem" alt={` joya ${nro}`}/>
           <div className="card-body">
-            <h5 className="card-title text-center p-2">COD : {nro}</h5>
-         
+            <h5 className="card-title text-center p-2">COD :{nro}</h5>
           </div>
         </div>
       </Link>
